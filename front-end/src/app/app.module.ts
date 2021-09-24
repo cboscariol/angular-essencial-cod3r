5d6779528058e1995165b75023df628aa4a8d564
cboscariol <caamila.boscariol@gmail.com>
2021-09-24 15:17:46 -0300

    create product method

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
 import { RedDirective } from './directives/red.directive';
 import { ForDirective } from './directives/for.directive';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -41,7 +41,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/components/product/product.model.ts
+++ front-end/src/app/components/product/product.model.ts
@@ -1,5 +1,5 @@
 export interface Product {
   id?: number // o ? quer dizer que o dado não é obrigatório
   name: string
-  price: number
+  price: any
 }
